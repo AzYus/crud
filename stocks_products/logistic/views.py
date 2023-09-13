b9266f3485d4f0369b0abd9b08b0ac5f49f165a4
Az Yu <azyulaml@gmail.com>
2023-09-13 23:22:39 +0200

    adj3

--- stocks_products/logistic/views.py
+++ stocks_products/logistic/views.py
@@ -1,8 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
-
+from django_filters.rest_framework import DjangoFilterBackend
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
 from rest_framework.filters import SearchFilter
+from rest_framework import filters
 from rest_framework.pagination import LimitOffsetPagination
 
 class ProductViewSet(ModelViewSet):
@@ -17,5 +18,6 @@ class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
     # при необходимости добавьте параметры фильтрации
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
     filterset_fields = ['products']
     pagination_class = LimitOffsetPagination
